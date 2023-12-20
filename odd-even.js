cbed861d3b4bf1c929728b57933ebc3d4dbc5b5b
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-20 12:26:07 +0200

    functioned

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,11 @@
 var checkNumer = 100;
 
-if (checkNumer % 2 == 0){
-    console.log(checkNumer + " is even number.");
+function checkNumerFunct(checkNumer){
+    if (checkNumer % 2 == 0){
+        console.log(checkNumer + " is even number.");
+    }
+    else
+        console.log(checkNumer + " is odd number.");
 }
-else
-    console.log(checkNumer + " is odd number.");
+
+checkNumerFunct(checkNumer);
