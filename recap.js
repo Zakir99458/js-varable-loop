161ed6573d279725d044ace1f44375b1ec3d9d1d
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-20 11:29:20 +0200

    first commit

---
+++ recap.js
@@ -0,0 +1,21 @@
+var time = "10:15PM";
+var bookPrice = 150;
+var isWhiteColor = false;
+
+// Arrary parameter
+var partners = ["Majid", "Sajid", "Kuddus", "Manik", "Heroine"];
+var elementCount = partners.length;
+console.log(elementCount);
+var elementIndex = partners.indexOf("Sajid");
+console.log(elementIndex);
+partners.push("I ama last");
+console.log(partners);
+partners.pop();
+console.log(partners);
+for(var i=0; i<partners.length; i++){
+    printPartners(partners[i])
+}
+function printPartners(partnerName){
+    console.log(partnerName);
+}
+
