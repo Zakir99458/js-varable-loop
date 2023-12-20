f83f4b45d22dafac4dc11f9250c1ebc4a277b923
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-20 22:24:36 +0200

    loop

---
+++ loop.js
@@ -0,0 +1,16 @@
+// Declare array
+var elementList = [10,6,22,12,30,11,50,33];
+
+// Create function to find the largest number
+function findLargestNumber(elementList){
+    largestNumber = 0;
+    for(let i=0; i<elementList.length; i++){
+        if(elementList[i]>largestNumber){
+            largestNumber = elementList[i]
+        }
+    }
+    return largestNumber;
+}
+
+// Print largest numbers
+console.log("Largest number is: " + findLargestNumber(elementList));
